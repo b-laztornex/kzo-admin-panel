c2d1522aec0fee8b7a25984a0cd2769eb364592b
Marco Casanova <m.casanova.dev@gmail.com>
2023-08-24 12:42:31 +0200

    add login && dashboard && boostrap

---
+++ src/hooks.server.js
@@ -0,0 +1,21 @@
+import { redirect } from '@sveltejs/kit';
+
+const unProtectedRoutes = ['/', '/login'];
+
+export const handle = async ({ event, resolve }) => {
+	const token = event.cookies.get('token');
+
+	if (!token && !unProtectedRoutes.includes(event.url.pathname)) {
+		throw redirect(303, '/');
+	}
+
+	const query = event.url.searchParams.get('signout');
+
+	if (Boolean(query) == true) {
+		await event.cookies.set('token', '', { path: '/', maxAge: 0 });
+		//await event.cookies.delete('token', { path: '/' });
+		throw redirect(303, '/login');
+	}
+
+	return resolve(event);
+};
